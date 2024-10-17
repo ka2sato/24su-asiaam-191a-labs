3331785685d0d008163ffa4f8dbc7623c9c99197
kazz24s <kazu.sato@17.almnni.u-tokyo.ac.jp>
2024-10-17 21:19:50 +0900

    1017

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,10 +1,29 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
-    center: [ -118.444, 34.0709], // Starting position [lng, lat]
+    style: 'https://api.maptiler.com/maps/satellite/style.json?key=7rnB2sjZJWhB4uUDzd8a', // Your style URL
+    center: [ 138.385391, 34.985828], // Starting position [lng, lat]
     zoom: 15 // Starting zoom level
 });
+
+let longitude = 138.385391;
+let latitude = 34.985828;
+
+// Add a marker to the map
+const marker = new maplibregl.Marker()
+    .setLngLat([longitude, latitude])
+    .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
+    .setHTML('Hello! This is an HTML popup, meaning I can add any <tags> I want here! Just be sure to close them </tags>'))
+    .addTo(map);
+
+    let longitude2 = 138.4;
+    let latitude2 = 35.0;
+// Add a marker to the map
+const marker2 = new maplibregl.Marker()
+.setLngLat([longitude2, latitude2])
+.setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
+.setHTML('<div class="awesomePopup">Hello! This is an HTML popup, meaning I can add any <tags> I want here! Just be sure to close them </tags></div>'))
+.addTo(map);
 
 // Add a marker to the map
 new maplibregl.Marker()
